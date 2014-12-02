bfb52d9d3e3f7c8134ce950de5d968bd3090077d
Kenner Kliemann <kenner.hp@gmail.com>
2014-12-02 11:50:39 -0200

    add rspec

--- lib/ruby_crc.rb
+++ lib/ruby_crc.rb
@@ -1,11 +1,11 @@
 module RubyCrc
   BIT_FLAG = 0x3E # 0b111110
 
-  PAYLOAD_IN = 'entrada'
-  PAYLOAD_OUT = 'saida'
-  FRAMES_OUT = 'transmissao'
+  PAYLOAD_IN = 'entrada' # Arquivo de entrada de dados
+  PAYLOAD_OUT = 'saida' # Arquivo de saida após o processamento
+  FRAMES_OUT = 'transmissao' # Arquivo com os frames a serem transmitidos
 
-  CRC_CODE = 0b10011
+  CRC_CODE = 0b10011 # polinomio gerador para o crc
 
   VERSION = '0.0.1'
 end
