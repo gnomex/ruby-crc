6a9a09cdf566bdacca940442a4eb8561fd65e44b
Kenner Kliemann <kenner.hp@gmail.com>
2014-12-03 16:47:10 -0200

    Draft

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -11,6 +11,50 @@ module RubyCrc
   ## Formato do log
   # => [OK|ERRO] payload [polinônimo gerador CRC]
   class Receiver
+    def initialize
+      @buffered_frames = ""
+      @stack = []
+      load_frames
+    end
 
+
+    private
+    def load_frames
+      File.open(RubyCrc::FRAMES_OUT, "r") do |f|
+        f.each_line do |line|
+          @buffered_frames << line.chop unless line.empty?
+        end
+      end
+    end
+
+    def parse
+
+    find a
+    if index a != 0 -> chunck -> push to chunks, status: undefined
+      remove and find next, if next == current + 1 -> shit!, update latest chuck to frame_error, ignore a
+    else
+      push to stack
+      remove a
+    end
+
+
+      # a.gsub(BIT_FLAG_REGEX, 'a')
+
+      # find 'a' from front of string
+      # remove them
+      # find next a
+      # copy content and push to stack
+      # remove a
+
+      # index('a')
+      # gsub(/^.a/)
+
+      # @stack.push
+      # @stack.pop
+    end
+
+    def resync
+
+    end
   end
 end
