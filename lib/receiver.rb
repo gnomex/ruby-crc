b68dd7dc095c24cc46365f5d1f5f2012bbcd97f9
Kenner Kliemann <kenner.hp@gmail.com>
2014-12-03 18:39:44 -0200

    Parsing and resync

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -22,20 +22,47 @@ module RubyCrc
     def load_frames
       File.open(RubyCrc::FRAMES_OUT, "r") do |f|
         f.each_line do |line|
-          @buffered_frames << line.chop unless line.empty?
+          # @buffered_frames << line.chop unless line.empty?
+          @buffered_frames << line unless line.empty?
         end
       end
     end
 
-    def parse
+    def self.parse(frames)
+      # buffer = @buffered_frames.gsub(BIT_FLAG_REGEX, 'a') # reserved buffer, replace flags to a
+      @stack = Array.new
 
-    find a
-    if index a != 0 -> chunck -> push to chunks, status: undefined
-      remove and find next, if next == current + 1 -> shit!, update latest chuck to frame_error, ignore a
-    else
-      push to stack
-      remove a
+      buffer = frames.gsub(BIT_FLAG_REGEX, 'a') # reserved buffer, replace flags to a
+
+      while buffer.length > 0
+
+        index = buffer.index('a')
+        puts "index. #{index}"
+        buffer = buffer.gsub(/^a/, '') #remove first
+
+        if index == 0
+
+          aux = buffer.index('a')
+
+          @stack.push( { data: buffer[0..(aux - 1)], status: 'ok' } )
+          buffer = buffer[aux..-1]
+        else
+          @stack.push( { data: buffer[0..(index - 1)], status: 'erro' } )
+          buffer = buffer[(index..-1)]
+        end
+
+        buffer = buffer.gsub(/^a/, '') #remove last
+      end
+      puts @stack.inspect
     end
+
+    # find a
+    # if index a != 0 -> chunck -> push to chunks, status: undefined
+    #   remove and find next, if next == current + 1 -> shit!, update latest chuck to frame_error, ignore a
+    # else
+    #   push to stack
+    #   remove a
+    # end
 
 
       # a.gsub(BIT_FLAG_REGEX, 'a')
@@ -51,10 +78,5 @@ module RubyCrc
 
       # @stack.push
       # @stack.pop
-    end
-
-    def resync
-
-    end
   end
 end
